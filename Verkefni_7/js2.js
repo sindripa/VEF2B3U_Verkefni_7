e569655c3d55321d0506feff046e2f3daa1e900c
sindripa <sindripa234@nemi.tskoli.is>
2017-02-27 17:10:32 +0000

    Add files via upload

---
+++ Verkefni_7/js2.js
@@ -0,0 +1,9 @@
+//dæmi 1.
+var x = document.getElementById('two');
+x.innerHTML = x.innerHTML.replace("Gunnar", "Jakob");
+
+//dæmi 2.
+document.getElementById('one').className = "active";
+
+//dæmi 3.
+document.getElementById('three').remove();
